27b619e6695d678a32ebe6850b360d44a5547ef8
Thomas van Tilburg <thomas@vantilburg.me>
2026-03-01 11:58:21 +0100

    Fix 2FA

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import crypto from "crypto";
 import {
   AddProductInput,
   ApiConfig,
-  ApiError,
   Article,
   Category,
+  ChannelType2FA,
   ConsentSetting,
   CountryCode,
   CustomerServiceContactInfo,
@@ -33,7 +33,6 @@ import {
   SuggestionResult,
   User,
   UserInfo,
-  Verify2FACodeInput,
   WalletTransaction,
   WalletTransactionDetails,
   WalletTransactionsInput,
@@ -45,6 +44,16 @@ export = class PicnicClient {
   apiVersion: string;
   authKey: string | null;
   url: string;
+
+  readonly baseHeaders = {
+    "User-Agent": "okhttp/3.12.2",
+    "Content-Type": "application/json; charset=UTF-8",
+  };
+
+  readonly picnicHeaders = {
+    "x-picnic-agent": "30100;1.15.232-15154",
+    "x-picnic-did": "3C417201548B2E3B",
+  };
 
   /**
    * Builds the client that sends the requests.
@@ -71,9 +80,7 @@ export = class PicnicClient {
 
     const response = await fetch(`${this.url}/user/login`, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: this.baseHeaders,
       body: JSON.stringify({
         key: username,
         secret,
@@ -103,9 +110,9 @@ export = class PicnicClient {
 
   /**
    * Generates a 2FA code for the user to verify.
-   * @param {string} channel The channel to send the code to. Can be 'SMS' or possibly something else.
+   * @param {string} channel The channel to send the code to. Can be 'SMS', 'EMAIL' or possibly something else.
    */
-  generate2FACode(channel: string) {
+  generate2FACode(channel: ChannelType2FA) {
     return this.sendRequest<Generate2FACodeInput, null>("POST", `/user/2fa/generate`, { channel }, true);
   }
 
@@ -113,8 +120,36 @@ export = class PicnicClient {
    * Verifies the 2FA code from the user.
    * @param {string} code The code to verify.
    */
-  verify2FACode(code: string) {
-    return this.sendRequest<Verify2FACodeInput, null | ApiError>("POST", `/user/2fa/verify`, { otp: code }, true);
+  async verify2FACode(code: string): Promise<any> {
+    const headers = new Headers({
+      ...this.baseHeaders,
+      ...this.picnicHeaders,
+      ...(this.authKey && { "x-picnic-auth": this.authKey }),
+    });
+
+    const response = await fetch(`${this.url}/user/2fa/verify`, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify({ otp: code }),
+    });
+
+    if (response.headers.has("x-picnic-auth")) {
+      this.authKey = response.headers.get("x-picnic-auth");
+    }
+
+    if (!response.ok) {
+      try {
+        const errorData: any = await response.json();
+        throw new Error(`${errorData.error?.message || response.statusText}`);
+      } catch (e) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+    }
+
+    const text = await response.text();
+    const data = text ? JSON.parse(text) : null;
+
+    return { ...data, authKey: this.authKey };
   }
 
   /**
@@ -465,10 +500,9 @@ export = class PicnicClient {
     isImageRequest: boolean = false
   ): Promise<TResponseData> {
     const headers = new Headers({
-      "User-Agent": "okhttp/3.12.2",
-      "Content-Type": "application/json; charset=UTF-8",
+      ...this.baseHeaders,
       ...(this.authKey && { "x-picnic-auth": this.authKey }),
-      ...(includePicnicHeaders && { "x-picnic-agent": "30100;1.15.232-15154", "x-picnic-did": "3C417201548B2E3B" }),
+      ...(includePicnicHeaders && this.picnicHeaders),
     });
 
     const response = await fetch(`${this.url}${path}`, {
